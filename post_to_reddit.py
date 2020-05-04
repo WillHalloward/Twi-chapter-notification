4c1bb831d583b1a79bd823d171f5abc78cfb1fe4
will_halloward <willhalloward@gmail.com>
2020-05-04 23:04:10 +0200

    Added a comment on the reddit discussion thread.

--- post_to_reddit.py
+++ post_to_reddit.py
@@ -12,4 +12,15 @@ def post_to_reddit(p_title, url):
 
     wanderingInn = reddit.subreddit("WanderingInn")
     title = "[Discussion] - " + p_title
-    wanderingInn.submit(title, url=url, flair_id="b8f4045a-f091-11e8-871f-0e4511bea43e")
+    post = wanderingInn.submit(title, url=url, flair_id="b8f4045a-f091-11e8-871f-0e4511bea43e")
+    post.reply(
+        """Hey folks.
+
+A reminder on the Patreon spoiler rules. **EVERYTHING** that's a part of the Patreon chapter
+is considered a spoiler that isn't public information.
+
+Also if you see what you think is a spoiler please use the report function and a mod will take a look at it.
+
+Thanks!
+
+Ps! Remember to vote for twi at http://topwebfiction.com/vote.php?for=the-wandering-inn""")
